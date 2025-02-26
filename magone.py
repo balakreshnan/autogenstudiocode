7a7f68a8c4fd64c2c7b92315f5d26853a94d670f
balakreshnan <babal@microsoft.com>
2025-02-26 10:56:47 -0600

    updated

--- magone.py
+++ magone.py
@@ -1,7 +1,8 @@
 import asyncio
+import sys
 from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
 from autogen_agentchat.agents import AssistantAgent
-from autogen_agentchat.teams import MagenticOneGroupChat
+from autogen_agentchat.teams import MagenticOneGroupChat, RoundRobinGroupChat
 from autogen_agentchat.ui import Console
 from autogen_ext.agents.web_surfer import MultimodalWebSurfer
 import os
@@ -10,6 +11,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 source_file_path = "./scode/"
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
 model_client = AzureOpenAIChatCompletionClient(model="gpt-4o",
                                                azure_deployment="gpt-4o-2", 
@@ -32,11 +36,28 @@ async def main() -> None:
     # await Console(team.run_stream(task="Provide a different proof for Fermat's Last Theorem"))
 
     surfer = MultimodalWebSurfer(
-        "WebSurfer",
+        "MultimodalWebSurfer",
         model_client=model_client,
+        downloads_folder="./downs",
+        debug_dir="./debug",
+        headless = False,
+        to_resize_viewport=True,
+        description="A web surfing assistant that can browse and interact with web pages.",
+        start_page="https://www.bing.com",  # Optional: Initial page
+        animate_actions=True,
+        browser_data_dir="./browser_data",
     )
-    team = MagenticOneGroupChat([surfer], model_client=model_client)
-    await Console(team.run_stream(task="What is the UV index in Melbourne today?"))
+    team = MagenticOneGroupChat([surfer], model_client=model_client, max_turns=3)
+    # Define a team
+    # team = RoundRobinGroupChat([surfer], max_turns=3)
+    #await Console(team.run_stream(task="Navigate to the AutoGen readme on GitHub."))
+    # await Console(team.run_stream(task="Summarize latest updates from Accenture newsroowm."))
+    # await Console(team.run_stream(task="Summarize latest news from venture beat all things in AI."))
+    await Console(team.run_stream(task="Summarize latest news from Techmeme all things in AI."))
+    await surfer.close()
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+    asyncio.run(main())
